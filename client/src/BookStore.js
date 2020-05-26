7da5e02ea1c50f7569ac1a04e130cd1f00c074f8
Evyatar Cohen <coevyatar@gmail.com>
2020-05-27 02:22:30 +0300

    Refactor app to have react-router and bootstrap nav

--- client/src/BookStore.js
+++ client/src/BookStore.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import BookList from './BookList';
-import Navbar from './Navbar';
 
 class BookStore extends Component{
   render(){
     return (
       <div className="App">
-        <Navbar/>
         <BookList/>
       </div>
     );
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Navbar, Nav } from 'react-bootstrap';
+
+import './Navbar.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import BookStore from './BookStore';
+import Login from './Login';
+import NotFound from './NotFound';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BookStore />
+    <Router>
+      <Navbar bg="gray" expand="lg" sticky="top">
+        <Navbar.Brand href="/">Book Store</Navbar.Brand>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/login">Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Switch>
+        <Route exact path="/" component={BookStore}/>
+        <Route path="/login" component={Login}/>
+
+        <Route component={NotFound}/>
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
